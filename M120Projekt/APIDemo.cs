0a0339a2014c0ab48732ac7aae6a117fe5f71f83
LucaSchere <luca@scherer.ml>
2019-02-28 10:28:16 +0100

    K2

---
+++ M120Projekt/APIDemo.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M120Projekt
+{
+    static class APIDemo
+    {
+        #region KlasseA
+        // Create
+        public static void DemoACreate()
+        {
+            Debug.Print("--- DemoACreate ---");
+            // KlasseA
+            Data.KlasseA klasseA1 = new Data.KlasseA();
+            klasseA1.TextAttribut = "Artikel 1";
+            klasseA1.DatumAttribut = DateTime.Today;
+            Int64 klasseA1Id = klasseA1.Erstellen();
+            Debug.Print("Artikel erstellt mit Id:" + klasseA1Id);
+        }
+        public static void DemoACreateKurz()
+        {
+            Data.KlasseA klasseA2 = new Data.KlasseA { TextAttribut = "Artikel 2", BooleanAttribut = true, DatumAttribut = DateTime.Today };
+            Int64 klasseA2Id = klasseA2.Erstellen();
+            Debug.Print("Artikel erstellt mit Id:" + klasseA2Id);
+        }
+
+        // Read
+        public static void DemoARead()
+        {
+            Debug.Print("--- DemoARead ---");
+            // Demo liest alle
+            foreach (Data.KlasseA klasseA in Data.KlasseA.LesenAlle())
+            {
+                Debug.Print("Artikel Id:" + klasseA.KlasseAId + " Name:" + klasseA.TextAttribut);
+            }
+        }
+        // Update
+        public static void DemoAUpdate()
+        {
+            Debug.Print("--- DemoAUpdate ---");
+            // KlasseA ändert Attribute
+            Data.KlasseA klasseA1 = Data.KlasseA.LesenID(1);
+            klasseA1.TextAttribut = "Artikel 1 nach Update";
+            klasseA1.Aktualisieren();
+        }
+        // Delete
+        public static void DemoADelete()
+        {
+            Debug.Print("--- DemoADelete ---");
+            Data.KlasseA.LesenID(1).Loeschen();
+            Debug.Print("Artikel mit Id 1 gelöscht");
+        }
+        #endregion
+    }
+}
---
+++ M120Projekt/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace M120Projekt
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            // Wichtig!
+            Data.Global.context = new Data.Context();
+            // Aufruf diverse APIDemo Methoden
+            APIDemo.DemoACreate();
+            APIDemo.DemoACreateKurz();
+            APIDemo.DemoARead();
+            APIDemo.DemoAUpdate();
+            APIDemo.DemoARead();
+            APIDemo.DemoADelete();
+        }
+    }
+}
